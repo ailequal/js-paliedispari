736e3bfd29ead217f5b39eae425ef9cc8b3ac9a7
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-13 16:11:42 +0100

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -7,7 +7,11 @@ alert('Welcome to palindroma checker');
 
 do {
   name = prompt('Insert a word and I\'ll tell you if it\'s palindroma (the checker is case sensitive). Stop it by typing \'stop\'');
-  console.log(isPalindroma(name));
+  if (isPalindroma(name)) {
+    console.log(name + ' is palindroma');
+  } else {
+    console.log(name + ' is not palindroma');
+  }
 } while (name !== 'stop');
 
 function isPalindroma(word) {
@@ -15,18 +19,17 @@ function isPalindroma(word) {
     reverse += word.charAt(i);
   }
   if (word === reverse) {
-    i = 0;
-    reverse = '';
     return true;
   } else {
-    i = 0;
-    reverse = '';
     return false;
   }
+  i = 0;
+  reverse = '';
 }
 
 
 // odd or even
+var play = '';
 var userPick = '';
 var userNumber = 0;
 var botNumber = 0;
@@ -35,33 +38,43 @@ var total = 0;
 alert('Welcome to odd or even');
 
 do {
-  userPick = prompt('Pick \'odd\' or \'even\'');
-} while (userPick !== 'odd' && userPick !== 'even');
-console.log('User picked ' + userPick);
+  play = prompt('Do you want to play? Reply with \'yes\' or \'no\'');
+} while (play !== 'yes' && play !== 'no');
 
-if (userPick === 'even') {
-  console.log('Bot picked odd')
+if (play === 'yes') {
+
+  do {
+    userPick = prompt('Pick \'odd\' or \'even\'');
+  } while (userPick !== 'odd' && userPick !== 'even');
+  console.log('User picked ' + userPick);
+
+  if (userPick === 'even') {
+    console.log('Bot picked odd')
+  } else {
+    console.log('Bot picked even')
+  }
+
+  do {
+    userNumber = parseInt(prompt('Pick a number between 1 and 5'));
+  } while (userNumber < 1 || userNumber > 5);
+  console.log('User picked ' + userNumber);
+
+  botNumber = getRandomInt(1, 6);
+  console.log('Bot picked ' + botNumber);
+
+  total = userNumber + botNumber;
+  if (total % 2 === 0 && userPick === 'even') {
+    console.log('User wins!!');
+  } else if (total % 2 === 0 && userPick === 'odd') {
+    console.log('Bot wins...');
+  } else if (total % 2 !== 0 && userPick === 'even') {
+    console.log('Bot wins...');
+  } else {
+    console.log('User wins!!');
+  }
+
 } else {
-  console.log('Bot picked even')
-}
-
-do {
-  userNumber = parseInt(prompt('Pick a number between 1 and 5'));
-} while (userNumber < 1 || userNumber > 5);
-console.log('User picked ' + userNumber);
-
-botNumber = getRandomInt(1, 6);
-console.log('Bot picked ' + botNumber);
-
-total = userNumber + botNumber;
-if (total % 2 === 0 && userPick === 'even') {
-  console.log('User wins!!');
-} else if (total % 2 === 0 && userPick === 'odd') {
-  console.log('Bot wins...');
-} else if (total % 2 !== 0 && userPick === 'even') {
-  console.log('Bot wins...');
-} else {
-  console.log('User wins!!');
+  console.log('Goodbye');
 }
 
 function getRandomInt(min, max) {

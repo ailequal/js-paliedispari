fc9582c33a42f4714d75cc461ed7aad76372dad2
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-13 15:55:40 +0100

    Finished odd or even

--- js/script.js
+++ js/script.js
@@ -27,7 +27,46 @@ function isPalindroma(word) {
 
 
 // odd or even
-// var userPick;
-// do {
-//   userPick = prompt('Pick \'odd\' or \'even\'');
-// } while (userPick !== 'odd' && userPick !== 'even');
+var userPick = '';
+var userNumber = 0;
+var botNumber = 0;
+var total = 0;
+
+alert('Welcome to odd or even');
+
+do {
+  userPick = prompt('Pick \'odd\' or \'even\'');
+} while (userPick !== 'odd' && userPick !== 'even');
+console.log('User picked ' + userPick);
+
+if (userPick === 'even') {
+  console.log('Bot picked odd')
+} else {
+  console.log('Bot picked even')
+}
+
+do {
+  userNumber = parseInt(prompt('Pick a number between 1 and 5'));
+} while (userNumber < 1 || userNumber > 5);
+console.log('User picked ' + userNumber);
+
+botNumber = getRandomInt(1, 6);
+console.log('Bot picked ' + botNumber);
+
+total = userNumber + botNumber;
+if (total % 2 === 0 && userPick === 'even') {
+  console.log('User wins!!');
+} else if (total % 2 === 0 && userPick === 'odd') {
+  console.log('Bot wins...');
+} else if (total % 2 !== 0 && userPick === 'even') {
+  console.log('Bot wins...');
+} else {
+  console.log('User wins!!');
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+  //The maximum is exclusive and the minimum is inclusive
+}

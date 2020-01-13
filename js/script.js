5520bcedd655428431a0ed73181acf53d29c9ee1
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-13 15:29:17 +0100

    Improved isPalindroma function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 // isPalindroma
-var name = prompt('Insert a word and I\'ll tell you if it\'s palindroma');
+var i = 0;
+var name = '';
 var reverse = '';
-console.log(isPalindroma(name));
+
+alert('Welcome to palindroma checker');
+
+do {
+  name = prompt('Insert a word and I\'ll tell you if it\'s palindroma (the checker is case sensitive). Stop it by typing \'stop\'');
+  console.log(isPalindroma(name));
+} while (name !== 'stop');
 
 function isPalindroma(word) {
-  for (var i = name.length - 1; i >= 0; i--) {
-    reverse += name.charAt(i);
+  for (i = word.length - 1; i >= 0; i--) {
+    reverse += word.charAt(i);
   }
   if (word === reverse) {
+    i = 0;
+    reverse = '';
     return true;
   } else {
+    i = 0;
+    reverse = '';
     return false;
   }
 }
+
+
+// odd or even
+// var userPick;
+// do {
+//   userPick = prompt('Pick \'odd\' or \'even\'');
+// } while (userPick !== 'odd' && userPick !== 'even');
